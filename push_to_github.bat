d639f687b421221d9b883abc7e00d978b5a6bb3c
sannidha-kmit <pandirlasrisannidha@gmail.com>
2026-02-13 08:51:16 +0530

    Delete push_to_github.bat

--- push_to_github.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "c:\Users\DELL\Downloads\Emerge-X-main\Emerge-X-main"
-git init
-git remote add origin https://github.com/keshavrathi07/Emerge-X.git
-git add .
-git commit -m "Pure AI Prediction System - Purified Backend & Polished UI"
-git push -u origin main
